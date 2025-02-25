c3e514fdd0fdb96515deeb2eb6ca8cff088812cd
kongkong653 <635214314@qq.com>
2025-02-25 23:10:39 +0800

    Update main.go

--- app/product/main.go
+++ app/product/main.go
@@ -9,7 +9,7 @@ import (
 	//"github.com/A1sca/Douyin-Mall-Go/app/order/biz/model"
 	"github.com/A1sca/Douyin-Mall-Go/app/product/biz/dal"
 	"github.com/A1sca/Douyin-Mall-Go/app/product/conf"
-	"github.com/A1sca/Douyin-Mall-Go/rpc_gen/kitex_gen/product/productcatalogservice"
+	"github.com/A1sca/Douyin-Mall-Go/rpc_gen/kitex_gen/product/kitex_gen/productcatalogservice"
 	"github.com/cloudwego/kitex/pkg/klog"
 	"github.com/cloudwego/kitex/pkg/rpcinfo"
 	"github.com/cloudwego/kitex/server"
